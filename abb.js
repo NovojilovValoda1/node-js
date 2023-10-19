035c5c33fc04fa8ea7470e88500cb4754f7f5f66
= <=>
2023-10-19 15:26:32 +0300

    the 2

---
+++ abb.js
@@ -0,0 +1,22 @@
+// const path = require("path");
+// const fs = require("fs");
+
+// console.log("path.basename(__filename)");
+// console.log("path.direname(__filename)");
+// console.log("path.extename(__filename)");
+// console.log("path.parse(__filename)");
+
+// const myFile = path.join(__dirname, "tp", "1.txt");
+// // fs.mkdir(path.join(__dirname, "tp"), function (err) {
+// //   if (err) console.log(err);
+// // });
+// // fs.writeFile(myFile, "Привет студенты!\n", function (err) {
+// //   if (err) console.log(err);
+// // });
+// fs.appendFile(myFile, "Пока студенты!", function (err) {
+//   if (err) console.log(err);
+// });
+// fs.readFile(myFile, "UTF-8", function (err, content) {
+//   if (err) console.log(err);
+//   console.log(content);
+// });
--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-hjggdfhj;
+const express = require("express");
+app = express();
+app.get("/", function (req, res) {
+  res.send("Запрос пришел!");
+});
+app.post("/", function (req, res) {
+  res.send("Запрос пришел в посылке!");
+});
+app.listen("3000", function () {
+  console.log("listening on", 3000);
+});
